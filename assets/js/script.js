12581a883db91173b4919fe63eb744d9f86a3753
Muhammad Ahsan <ahsanirfan961@gmail.com>
2024-07-12 03:35:47 +0500

    Data Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function () {
     addListenersAll();
+    loadData();
 });
 
 function addListenersAll() {
@@ -38,4 +39,60 @@ function addRevealElementsListener() {
 
 window.go_to_cv = function () {
     window.open("https://drive.google.com/file/d/17emMlVaYCCEqZ0LEHXOcbEcGct1KNPqs/view?usp=sharing", '_blank');
+}
+
+
+function loadData() {
+    return fetch('data.json').then(response => response.json()).then(data => {
+        // Front Page
+        $('#name').text(data.name);
+        $('#quote').text(data.quote);
+
+        // About
+        $('#front_page_description').html(data.front_page_description);
+
+        // Services
+        services_section = $('#services-section');
+        template = $('.service');
+        services_section.empty();
+        console.log(template.html());
+        data.services.forEach(service => {
+            let serviceElement = template.clone();
+            serviceElement.find('.service-logo').attr('src', service.logo_path);
+            serviceElement.find('.service-title').text(service.title);
+            serviceElement.find('.service-description').text(service.description);
+            services_section.append(serviceElement);
+        });
+
+        // Skills
+        skills_section = $('#skill-section');
+        template = $('.skill');
+        skills_section.empty();
+        data.skills.forEach(skill => {
+            let skillElement = template.clone();
+            skillElement.find('.skill-percentage').html(skill.percentage + '<sup class="text-white fs-1">%</sup>');
+            skillElement.find('.skill-title').text(skill.name);
+            skills_section.append(skillElement);
+        });
+
+        // Experience
+        experience_section = $('#experience-section').find('#experience-contents');
+        template_1 = $('.experience-1');
+        template_2 = $('.experience-2');
+        experience_section.empty();
+        data.experience.forEach((experience, index) => {
+            let experienceElement = index % 2 === 0 ? template_1.clone() : template_2.clone();
+            experienceElement.find('.experience-logo').attr('src', experience.logo_path);
+            experienceElement.find('.experience-title').text(experience.organization);
+            duration = `From ${experience.from} to ${experience.to}`;
+            experienceElement.find('.experience-duration').text(duration);
+            experienceElement.find('.experience-position').text(experience.position);
+            experienceElement.find('.experience-mode').text(experience.mode);
+            experienceElement.find('.experience-location').text(experience.location);
+            experience_section.append(experienceElement);
+        });
+
+    }).catch(err => {
+        console.error(err);
+    });
 }
